830cc1e6ed77441ff6fb79c11a95019c49c21e05
abdulk90 <abdul.kayyali@gmail.com>
2016-06-01 14:08:20 -0400

    added comments model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160527121723) do
+ActiveRecord::Schema.define(version: 20160601180659) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -20,12 +20,14 @@ ActiveRecord::Schema.define(version: 20160527121723) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "date"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "articles_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
+
+  add_index "comments", ["articles_id"], name: "index_comments_on_articles_id"
 
 end
