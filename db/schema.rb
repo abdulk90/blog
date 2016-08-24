b712aa265fbd6d86abb8f1b06ee2a341ac500674
Abdul Kayyali <abdul.kayyali@gmail.com>
2016-08-24 17:43:51 -0400

    comments not working

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623200306) do
+ActiveRecord::Schema.define(version: 20160824212440) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "comments"
   end
 
   create_table "comments", force: :cascade do |t|
